4e980897d0044f05da63a85e8002050dbb1020e1
Yash Raj <212myashraj@gmail.com>
2026-02-16 11:13:22 +0530

    .

---
+++ Promise/async.js
@@ -0,0 +1,22 @@
+function fatchData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const data = { id: 1, name: "Yash Raj" };
+            resolve(data);
+        }, 2000);
+    });
+}
+
+async function getData() {
+    console.log("Fetching data...");
+    try {
+        const result = await fatchData();
+        console.log("Data received:", result);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    } finally {
+        console.log("Data fetching process completed.");
+    }
+}
+
+getData();
