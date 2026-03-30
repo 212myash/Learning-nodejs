bc59335faf6b3a4266edb51d25da8322a6bd195a
Yash Raj <212myashraj@gmail.com>
2026-03-30 15:34:45 +0530

    Implement Socket.IO chat server with message broadcasting

---
+++ sockets/socket-server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log("User connected");
+
+  socket.on('message', (msg) => {
+    console.log("Message:", msg);
+    io.emit('message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Socket server running");
+});
